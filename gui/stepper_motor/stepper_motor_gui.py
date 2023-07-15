1d0e5bcb9734591203ff7bf6f92870fe1627f1af
khoipkv <khoipkv@gmail.com>
2023-07-15 12:58:33 -0400

    stepper motor

---
+++ gui/stepper_motor/stepper_motor_gui.py
@@ -0,0 +1,128 @@
+import os
+
+from gui.guibase import GUIBase
+from core.connector import Connector
+from qtpy import QtCore
+from qtpy import QtGui
+from qtpy import QtWidgets
+from qtpy import uic
+from gui.colordefs import QudiPalettePale as palette
+
+class StepperMainWindow(QtWidgets.QMainWindow):
+    """ Create the Main Window based on the *.ui file. """
+
+    def __init__(self):
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'ui_stepper_motor.ui')
+
+        # Load it
+        super().__init__()
+        uic.loadUi(ui_file, self)
+        self.show()
+
+class PiezoGUI(GUIBase):
+    """
+
+    """
+    
+    # CONNECTORS #############################################################
+    aptlogic = Connector(interface='APTpiezoLogic')
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+    def on_deactivate(self):
+        """ Reverse steps of activation
+
+        @return int: error code (0:OK, -1:error)
+        """
+        self._mw.close()
+        #return 0
+
+    def on_activate(self):
+        """ 
+
+        """
+
+        # CONNECTORS PART 2 ###################################################
+        self._aptlogic = self.aptlogic()
+
+        self._mw = PiezoMainWindow()
+
+        # Set default parameters
+        self.position = [0, 0, 0]
+        self._mw.StepSize.setValue(10)
+        self.stepSize = 10
+        self.is_manual = True
+
+        # Connect buttons to functions
+        self._mw.StepSize.valueChanged.connect(self.stepChanged)
+        # self._mw.inputX.valueChanged.connect(self.manualInput)
+        # self._mw.inputY.valueChanged.connect(self.manualInput)
+        # self._mw.inputZ.valueChanged.connect(self.manualInput)
+
+        self._mw.upButton.clicked.connect(lambda: self.move(1,1))
+        self._mw.downButton.clicked.connect(lambda: self.move(1,-1))
+        self._mw.leftButton.clicked.connect(lambda: self.move(0,-1))
+        self._mw.rightButton.clicked.connect(lambda: self.move(0,1))
+        self._mw.zUpButton.clicked.connect(lambda: self.move(2,1))
+        self._mw.zDownButton.clicked.connect(lambda: self.move(2,-1))
+        self._mw.switchMode.clicked.connect(self.updateButton)
+
+        self._mw.moveManual.clicked.connect(self.manualInput)
+
+        # Connect update signal
+        self._aptlogic.sigUpdateDisplay.connect(self.updateDisplay)
+        self.show()
+
+
+    def move(self, axis, direction):
+        """Move piezo
+
+        Args:
+            axis (int): 0->x, 1->y, 2->z
+            direction (int, optional): Step direction. Defaults to 1.
+        """
+        if (not self.is_manual):
+            position = self.position
+            position[axis] = self.position[axis] + self.stepSize * direction
+
+            self._aptlogic.setPosition(position)
+
+
+    def stepChanged(self):
+        self.stepSize = self._mw.StepSize.value()
+
+    def manualInput(self):
+        if (self.is_manual):
+            position = [self._mw.inputX.value(), self._mw.inputY.value(), self._mw.inputZ.value()]
+            self.position = position
+            self._aptlogic.setPosition(position)
+
+
+    def updateDisplay(self):
+        self.position = self._aptlogic.position
+        self._mw.xVal.setText(str(self.position[0]))
+        self._mw.yVal.setText(str(self.position[1]))
+        self._mw.zVal.setText(str(self.position[2]))
+        if (not self.is_manual):
+            self._mw.inputX.setValue(self.position[0])
+            self._mw.inputY.setValue(self.position[1])
+            self._mw.inputZ.setValue(self.position[2])
+
+    def show(self):
+        """Make main window visible and put it above all other windows. """
+        # Show the Main GUI:
+        self._mw.show()
+        self._mw.activateWindow()
+        self._mw.raise_()
+
+
+    def updateButton(self):
+        if (self.is_manual == False):
+            self.is_manual = True
+            self._mw.switchMode.setText("Move by Input")
+        else:
+            self.is_manual = False
+            self._mw.switchMode.setText("Move by Step")
---
+++ hardware/motor/stepper_motor.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+"""
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+from PyQt5.QtCore import QObject
+from core.module import Base
+from hardware.motor.stepper import Stepper
+from core.configoption import ConfigOption
+
+import pyfirmata
+
+
+class StepperMotor (Base):
+    # def __init__(self):
+    #     super().__init__()
+    _motor_pin_1 = ConfigOption(name='motor_pin_1', missing='error')
+    _motor_pin_2 = ConfigOption(name='motor_pin_2', missing='error')
+    _motor_pin_3 = ConfigOption(name='motor_pin_3', missing='error')
+    _motor_pin_4 = ConfigOption(name='motor_pin_4', missing='error')
+    
+    
+    def on_activate(self):
+
+        self.board = pyfirmata.Arduino('COM7') # AUTOMATE
+        self.stepsPerRevolution = 2048
+        self.rpm = 12
+        self.position = 0
+
+        self._Stepper = Stepper(self.board, self.stepsPerRevolution, self._motor_pin_1, self._motor_pin_3, self._motor_pin_2, self._motor_pin_4)
+        self._Stepper.setSpeed(self.rpm)
+
+
+    def on_deactivate(self):
+        # self.move_abs(0)
+        return
+
+    
+    def move_abs(self, revolution):
+        """ Moves stage to absolute revolution (absolute movement)
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if (revolution > 0):
+            self._Stepper.step(revolution - self.revolution)
+            self.revolution = revolution
+            return 0
+        else: return -1
+
+
+    def move_rel(self,  direction, step=1):
+        """ Moves stage in given direction (relative movement)
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+        A smart idea would be to ask the position after the movement.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if (direction == 1):
+            self._Stepper.step(step)
+            self.position += step
+        else:
+            self._Stepper.step(-step)
+            self.position -= step
+
+
+    def get_pos(self):
+        """ Gets current position of the stage arms
+
+        @param list param_list: optional, if a specific position of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the
+                                position is asked.
+
+        @return dict: with keys being the axis labels and item the current
+                      position.
+        """
+        return self.position
+
+
+    def get_rpm(self):
+        """ Gets the current velocity for all connected axes.
+
+        @param dict param_list: optional, if a specific velocity of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed as the param_list.
+                                If nothing is passed, then from each axis the
+                                velocity is asked.
+
+        @return dict : with the axis label as key and the velocity as item.
+        """
+        return self.rpm
+
+
+    def set_rpm(self, rpm):
+        """ Write new value for velocity.
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-velocity-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        self._Stepper.setSpeed(rpm)
+        self.rpm = rpm
+
